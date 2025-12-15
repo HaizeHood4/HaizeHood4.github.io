12b6aff21cb45c5dcd207956063fd11be8152c42
HaizeHood4 <hood423@wearescpps.org>
2025-12-15 16:27:52 +0000

    runtime

--- fsd-projects/runtime/js/runLevels.js
+++ fsd-projects/runtime/js/runLevels.js
@@ -18,17 +18,32 @@ var runLevels = function (window) {
 
     // TODOs 5 through 11 go here
     // BEGIN EDITING YOUR CODE HERE
-    var hitZoneSize = 25;
-    var damageFromObstacle = 10;
-    var sawBladeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle);
-    sawBladeHitZone.x = 400;
-    sawBladeHitZone.y = 400;
-    game.addGameItem(sawBladeHitZone);
+    function createSawBlade(x, y) {
+      var hitZoneSize = 25;
+      var damageFromObstacle = 10;
+      var sawBladeHitZone = game.createObstacle(
+        hitZoneSize,
+        damageFromObstacle
+      );
+      sawBladeHitZone.x = x;
+      sawBladeHitZone.y = y;
+      game.addGameItem(sawBladeHitZone);
 
-    var obstacleImage = draw.bitmap("img/sawblade.png");
-    sawBladeHitZone.addChild(obstacleImage);
+      var obstacleImage = draw.bitmap("img/sawblade.png");
+      sawBladeHitZone.addChild(obstacleImage);
+    }
+    createSawBlade(700, 400);
+    createSawBlade(400, 450);
+    createSawBlade(950, 350);
 
-    createSawBlade 
+    var enemy = game.createdGameItem("enemy", 25);
+    var redSquare = draw.rect(50, 50, "red"); 
+    redSquare.x = -25;
+    redSquare.y = -25;
+    enemy.addChild(redSquare);
+    enemy.x = 400; 
+    enemy.y = groundY - 50;
+    game.addGameItem(enemy);
 
     function startLevel() {
       // TODO 13 goes below here
